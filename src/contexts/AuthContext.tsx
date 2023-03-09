78a3cc1225b8ba410722c966ab3e622424c3207b
Rilson Oliveira <rilson2.0@gmail.com>
2023-03-09 17:12:39 -0300

    Feito todas as implementações de signIn, signUp e signOut.

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,83 @@
+import { Children, createContext, ReactNode, useEffect, useState } from 'react';
+import { UserDTO } from '@dtos/UserDTO';
+import { api } from '@services/api';
+import {
+  storageUserGet,
+  storageUserRemove,
+  storageUserSave,
+} from '@storage/storageUser';
+
+export type AuthContextDataProps = {
+  user: UserDTO;
+  signIn: (email: string, password: string) => Promise<void>;
+  signOut: () => Promise<void>;
+  isLoadingUserStorageData: boolean;
+};
+
+type AuthContextProviderProps = {
+  children: ReactNode;
+};
+
+const AuthContext = createContext<AuthContextDataProps>(
+  {} as AuthContextDataProps
+);
+
+function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState<UserDTO>({} as UserDTO);
+  const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
+    useState(true);
+
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post('/sessions', { email, password });
+
+      if (data.user) {
+        setUser(data.user);
+        storageUserSave(data.user);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function signOut() {
+    try {
+      setIsLoadingUserStorageData(true);
+      setUser({} as UserDTO);
+
+      await storageUserRemove();
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
+  async function loadUserData() {
+    try {
+      const userLogged = await storageUserGet();
+
+      if (userLogged) {
+        setUser(userLogged);
+      }
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ user, signIn, signOut, isLoadingUserStorageData }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthContext, AuthContextProvider };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,25 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { AuthRoutes } from './auth.routes';
 import { AppRoutes } from './app.routes';
 
+import { AuthContext } from '@contexts/AuthContext';
+import { useAuth } from '@hooks/useAuth';
+import { Loading } from '@components/Loading';
+
 export function Routes() {
   const { colors } = useTheme();
+  const { user, isLoadingUserStorageData } = useAuth();
+
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
+
+  if (isLoadingUserStorageData) {
+    return <Loading />;
+  }
 
   return (
     <Box flex={1} bg='gray.700'>
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
