290a887a503b7948d30cb2511dfebe75ee5598df
Rilson Oliveira <rilson2.0@gmail.com>
2023-05-27 17:48:19 -0300

    feat:  Implements OneSignal configs.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'react-native';
+import { Platform, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import {
   useFonts,
@@ -8,8 +8,21 @@ import {
 import { THEME } from './src/theme';
 import { Loading } from '@components/Loading';
 import { Routes } from './src/routes';
+import OneSignal from 'react-native-onesignal';
 
 import { AuthContextProvider } from '@contexts/AuthContext';
+
+const { ONE_SIGNAL_APP_ID_ANDROID } = process.env;
+const { ONE_SIGNAL_APP_ID_IOS } = process.env;
+
+const oneSignalAppId =
+  Platform.OS === 'ios' ? ONE_SIGNAL_APP_ID_IOS : ONE_SIGNAL_APP_ID_ANDROID;
+
+OneSignal.setAppId(oneSignalAppId as string);
+
+OneSignal.promptForPushNotificationsWithUserResponse((response) => {
+  // console.log(response);
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
