427e36fff9e959b77207ed42d2c32e3d3c75b4ef
Rilson Oliveira <rilson2.0@gmail.com>
2023-05-27 17:53:34 -0300

    feat:  Create consts to OneSignal tags.

---
+++ src/notifications/notificationTagsConfig.ts
@@ -0,0 +1,5 @@
+const USER_AUTH_STATUS = 'user_auth_status';
+const LAST_EXERCISES_DATE = 'last_exercises_date';
+const WEEK_EXERCISES_COUNT = 'week_exercises_count';
+
+export { USER_AUTH_STATUS, LAST_EXERCISES_DATE, WEEK_EXERCISES_COUNT };
